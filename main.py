318242ec9d925fe6446a34bdd442fc9ff61bc62d
SOMETHINGGGG <105470748+SOMETHINGGGG@users.noreply.github.com>
2023-05-18 12:31:48 +1200

    Added customer entry

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hello")
+import ttkbootstrap as ttkb
+from ttkbootstrap.constants import *
+
+root = ttkb.Window()
+
+customer_label = ttkb.Label(root,text='Customer name:')
+customer_label.config(font=('Helvetica',18))
+customer_label.grid(row=1,column=0,padx=10,pady=5)
+
+customer_entry = ttkb.Entry(root)
+customer_entry.grid(row=1,column=1,padx=10,pady=5)
+
+root.mainloop()
